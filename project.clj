73844d6b4c97319b5e5dcd602bbb319187f01d7e
Geoff Shannon <geoffpshannon@gmail.com>
2013-12-12 19:17:11 -0800

    Switch to logback framework

--- project.clj
+++ project.clj
@@ -7,10 +7,11 @@
   :main wall_follower.Main
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
+  :resource-paths ["resource"]
   :aliases {"run-ev3" ["do" "uberjar,"
                        "shell" "scp" "target/ev3main.jar" "root@10.0.1.1:,"
                        "shell" "ssh" "root@10.0.1.1" "/bin/jrun -jar /home/root/ev3main.jar"]
             "shutdown-ev3" ["shell" "ssh" "root@10.0.1.1" "halt"]}
   :dependencies [[org.clojars.earthlingzephyr/lejos-ev3 "0.5.0-SNAPSHOT"]
-                 [org.slf4j/slf4j-jdk14 "1.7.5"]]
+                 [ch.qos.logback/logback-classic "1.0.13"]]
   :plugins [[lein-shell "0.3.0"]])
