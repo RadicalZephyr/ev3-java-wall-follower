e3e3144d44c93b3eab06623e938300bb9bb1574c
Geoff Shannon <geoffpshannon@gmail.com>
2013-12-12 17:41:12 -0800

    Add slf4j as a dependency

--- project.clj
+++ project.clj
@@ -11,5 +11,6 @@
                        "shell" "scp" "target/ev3main.jar" "root@10.0.1.1:,"
                        "shell" "ssh" "root@10.0.1.1" "/bin/jrun -jar /home/root/ev3main.jar"]
             "shutdown-ev3" ["shell" "ssh" "root@10.0.1.1" "halt"]}
-  :dependencies [[org.clojars.earthlingzephyr/lejos-ev3 "0.5.0-SNAPSHOT"]]
+  :dependencies [[org.clojars.earthlingzephyr/lejos-ev3 "0.5.0-SNAPSHOT"]
+                 [org.slf4j/slf4j-jdk14 "1.7.5"]]
   :plugins [[lein-shell "0.3.0"]])
